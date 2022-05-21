453beee2e79a9323b3d55e32690de6d5de0f7c07
Анна <lilimp8@gmail.com>
2022-05-21 15:08:46 +0600

    Добавляет оптимизацию графики страниц

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,11 @@
 const mainNavigation = document.querySelector(".main-nav");
 const navigationToggleButton = document.querySelector(".main-nav__toggle");
+const googleMap = document.querySelector(".contacts__google-map");
+const imageMap = document.querySelector(".contacts__image-map");
 
 mainNavigation.classList.remove("main-nav--nojs");
+googleMap.classList.remove("contacts__google-map--nojs");
+imageMap.classList.add("contacts__image-map--js");
 
 navigationToggleButton.addEventListener("click", function () {
   if (mainNavigation.classList.contains("main-nav--closed")) {
